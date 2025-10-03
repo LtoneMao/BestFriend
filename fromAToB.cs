e1dac2ee4403e356e4eb67cce32e4ce30ab4219b
LtoneMao <yitongduan@foxmail.com>
2025-10-03 19:06:16 +0800

    add First File

---
+++ fromAToB.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fromAToB : MonoBehaviour
+{
+    public bool playerEnter = false;
+    public KeyCode interactButton;
+    public GameObject targetPoint;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(interactButton) && playerEnter)
+        {
+            //state
+
+        }
+
+        
+        // ÒÆ¶¯ÎïÌå
+        transform.position = Vector2.MoveTowards(transform.position, targetPoint.transform.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playerEnter = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerEnter = false;
+        }
+    }
+}
